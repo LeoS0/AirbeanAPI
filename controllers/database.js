0245e1b44c02137fb16f693c109224eb2d7728be
Leo <lenti11@live.se>
2021-04-30 13:01:15 +0200

    Fixes

--- controllers/database.js
+++ controllers/database.js
@@ -4,8 +4,61 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('database.json');
 const database = lowdb(adapter);
 
+const moment = require('moment');
+
 function initiateDatabase() {
-  database.defaults({ accounts: [] }).write();
+  database.defaults({ accounts: [], orders: [] }).write();
 }
 
+function addAccount(body) {
+  const account = body;
+  console.log('Account Info:', account);
+
+  const usernameExists = database.get('accounts').find({ username: account.username }).value();
+  const passwordExists = database.get('accounts').find({ password: account.password }).value();
+
+  let result = false;
+
+  if (!usernameExists && !passwordExists) {
+    database.get('accounts').push(account).write();
+    result = true;
+  }
+
+  return result;
+}
+
+function addOrder(body) {
+  const order = body;
+  const orderID = 1;
+  const eta = Math.floor(Math.random() * 10);
+
+  database.get('orders').push({ orderID: orderID, menuID: order.menuID, userID: order.userID, eta: eta }).write();
+
+  return 'Done';
+}
+
+/*
+{
+  accounts: 
+  [
+    {
+      userID: 1
+      username: test
+      password: test,
+      orderID: 1923
+    }
+  ],
+  orders: [
+    {
+      orderID: 1923,
+      menuID: 2,
+      userID: 1,
+      eta: 3
+    }
+  ]
+}
+*/
+
 exports.initiateDatabase = initiateDatabase;
+exports.addAccount = addAccount;
+exports.addOrder = addOrder;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
 const router = new Router();
 
+const menu = require('../menu.json');
+const { addAccount, addOrder } = require('../controllers/database');
+
 router.get('/coffee', (req, res) => {
-  console.log(req);
-  res.send('Test');
+  res.send(menu);
+});
+
+router.post('/account', (req, res) => {
+  res.json(addAccount(req.body));
+});
+
+router.post('/order', (req, res) => {
+  res.json(addOrder(req.body));
 });
 
 module.exports = router;
